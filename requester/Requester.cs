a37a9f75e0a8c8c41deab9a4e7cfec4e9fb4d947
Ekin Bulut <ekinbulut@gmail.com>
2023-08-14 12:25:55 +0300

    initials

---
+++ requester/Requester.cs
@@ -0,0 +1,41 @@
+﻿namespace requester;
+
+public class Requester
+{
+    private string _url;
+    private string _authKey;
+    private string _username, _password;
+    
+    public Requester(string url)
+    {
+        _url = url;
+    }
+
+    public Requester(string url, string withAuthKey)
+    {
+        _url = url;
+        _authKey = withAuthKey;
+    }
+    
+    public Requester(string url, string username, string password)
+    {
+        _url = url;
+        _username = username;
+        _password = password;
+    }
+    
+    public T MakeRequest<T>(HttpMethod httpMethod) where T: new()
+    {
+        var result = new T();
+        
+        return result;
+    }
+
+
+    public async Task<T> MakeRequestAsync<T>() where T : new()
+    {
+        var result = new T();
+        
+        return result;
+    }
+}
